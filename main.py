e755941abfc926d8a9cbdb69b053572143efbbc9
Sama <s.m.alfadhli@gmail.com>
2022-05-10 17:00:53 +0300

    everything working

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def encrypt(myList):
     sentence = ''
     for i in range(len(sentenceTemp)-1):
         sentence += sentenceTemp[i]
-        if(sentenceTemp[i+1] == sentenceTemp[i]):
+        if(i % 2 == 0 and sentenceTemp[i+1] == sentenceTemp[i]):
             sentence += 'x'
     sentence += sentenceTemp[len(sentenceTemp)-1]
     if(len(sentence) % 2 == 1):
@@ -61,14 +61,59 @@ def encrypt(myList):
                 cipher += myList[secondIndex[0] +
                                  1 if secondIndex[0] < 4 else 0][secondIndex[1]]
             # for angle switching
-            cipher += myList[firstIndex[0]][secondIndex[1]]
-            cipher += myList[secondIndex[0]][firstIndex[1]]
-
-    print(cipher)
+            else:
+                cipher += myList[firstIndex[0]][secondIndex[1]]
+                cipher += myList[secondIndex[0]][firstIndex[1]]
+    print(sentence)
+    print('Cipher output:', cipher)
 
 
 def decrypt(myList):
-    sentence = input("Enter cipher input> ")
+    sentenceTemp = re.sub(
+        r'[^a-zA-Z]', '', input("Enter Cipher input> ").lower())
+    if(len(sentenceTemp) <= 0):
+        return
+    sentence = sentenceTemp
+
+    cipher = ''
+    i = 0
+    while(i < len(sentence)-1):
+        firstLetter = sentence[i].upper()
+        secondLetter = sentence[i+1].upper()
+        firstIndex = [-1, -1]
+        secondIndex = [-1, -1]
+        # check if in the same row
+        isfound = False
+        for row in myList:
+            if(firstLetter in row and secondLetter in row):
+                # shift them right
+                isfound = True
+                cipher += row[row.index(firstLetter) -
+                              1 if row.index(firstLetter) > 0 else 4]
+                cipher += row[row.index(secondLetter) -
+                              1 if row.index(secondLetter) > 0 else 4]
+            elif(firstLetter in row or secondLetter in row):
+                if firstLetter in row:
+                    firstIndex = [myList.index(row), row.index(
+                        firstLetter)]
+                elif secondLetter in row:
+                    secondIndex = [myList.index(row), row.index(
+                        secondLetter)]
+
+        if(not isfound):
+            # check if in the same column
+            if(firstIndex[1] == secondIndex[1]):
+                cipher += myList[firstIndex[0] -
+                                 1 if firstIndex[0] > 0 else 4][firstIndex[1]]
+                cipher += myList[secondIndex[0] -
+                                 1 if secondIndex[0] > 0 else 4][secondIndex[1]]
+            # for angle switching
+            else:
+                cipher += myList[firstIndex[0]][secondIndex[1]]
+                cipher += myList[secondIndex[0]][firstIndex[1]]
+        i += 2
+
+    print('Plaintext output:', cipher)
 
 
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L",
